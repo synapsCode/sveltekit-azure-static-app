ee6bd4d137ea9d383b1892950c363af8b51d9739
Mirek Kędzierski <miroslaw.kedzierski@acoustic.co>
2023-10-21 07:03:16 +0200

    fix: add pagination, remove about page

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -8,9 +8,6 @@
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
 		</ul>
 	</nav>
 </header>
